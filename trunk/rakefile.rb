8ba327d46cfe83be17844c1996f9a021a5d4ad1a
joshuacronemeyer <joshuacronemeyer@6f2a6cbf-f331-0410-ba9f-d30d2cb46178>
2007-07-02 22:36:38 +0000

    i guess this works.

--- trunk/rakefile.rb
+++ trunk/rakefile.rb
@@ -1,9 +1,10 @@
 task :default => [:test]
 task :cruise => [:test]
 task :test do |t|
-#				    exit 1
 	test_files = FileList['*test*']
 	test_files.each do |file| 
-		system("ruby #{file}")
+		if (system("ruby #{file}")) !=0
+			exit 1
+		end
 	end
 end
