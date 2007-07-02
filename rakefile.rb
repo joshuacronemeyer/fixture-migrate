f05dfebfb0a997f6601b3af69c5244bff7744551
joshuacronemeyer <joshuacronemeyer@6f2a6cbf-f331-0410-ba9f-d30d2cb46178>
2007-07-02 22:32:54 +0000

    trying to get a more legit failure

--- rakefile.rb
+++ rakefile.rb
@@ -1,7 +1,11 @@
 task :default => [:test]
 task :cruise => [:test]
 task :test do |t|
-				    exit 1
-#  test_files = FileList['*test*']
-#	test_files.each{|file| system("ruby #{file}")}
+#				    exit 1
+	test_files = FileList['*test*']
+	test_files.each do |file| 
+		if (system("ruby #{file}")) != 0
+			exit 1
+		end
+	end
 end
